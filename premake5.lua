30ed1c3fb52db70fb13c1527cb8955e7c214db76
Aryan Sinha <aryananusinha@gmail.com>
2022-07-04 21:40:58 -0400

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+project "EABase"
+    kind "None"
+    language "C++"
+    staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "include/**.h"
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+    defines
+    {
+        "_CHAR16T",
+        "_CRT_SECURE_NO_WARNINGS"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
